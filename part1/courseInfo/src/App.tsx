dbd5879f21aa7880860a7500eff40530bcd9df23
Waseem Mansour <waseem.mn2002@gmail.com>
2024-03-10 20:54:32 +0400

    Modify to match Part 1 Exercises 1.3 - 1.5

--- part1/courseInfo/src/App.tsx
+++ part1/courseInfo/src/App.tsx
@@ -1,22 +1,24 @@
 import {Header} from "./components/Header";
 import {Content} from "./components/Content";
-import {CoursePart} from "../types";
+import {Course} from "./types";
 import {Total} from "./components/Total";
 
 
 const App = () => {
-  const courseTitle = 'Half Stack application development'
-  const courseParts: CoursePart[] =  [
-    { name: 'Fundamentals of React', numOfExercises: 10 },
-    { name: 'Using props to pass data', numOfExercises: 7 },
-    { name: 'State of a component', numOfExercises: 14 },
-  ]
+  const course: Course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', numOfExercises: 10 },
+      { name: 'Using props to pass data', numOfExercises: 7 },
+      { name: 'State of a component', numOfExercises: 14 },
+    ]
+  }
 
   return (
     <div>
-      <Header courseTitle={courseTitle} />
-      <Content courseParts={courseParts} />
-      <Total courseParts={courseParts} />
+      <Header courseTitle={course.name} />
+      <Content courseParts={course.parts} />
+      <Total courseParts={course.parts} />
     </div>
   )
 }
--- part1/courseInfo/src/types/index.tsx
+++ part1/courseInfo/src/types/index.tsx
@@ -2,3 +2,8 @@ export type CoursePart = {
   name: string;
   numOfExercises: number;
 }
+
+export type Course = {
+  name: string;
+  parts: CoursePart[]
+}
