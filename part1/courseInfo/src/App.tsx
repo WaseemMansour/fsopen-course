c77d7289450e9d4ea7a775f467bbca270f64d723
Waseem Mansour <waseem.mn2002@gmail.com>
2024-03-23 01:55:17 +0400

    refactor the app

--- part1/courseInfo/src/App.tsx
+++ part1/courseInfo/src/App.tsx
@@ -2,9 +2,17 @@ import {Header} from "./components/Header";
 import {Content} from "./components/Content";
 import {Course} from "./types";
 import {Total} from "./components/Total";
+import {useState} from "react";
 
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  
+  const setToZero = () => setCounter(0)
+
+  console.log('rendering...', counter)
   const course: Course = {
     name: 'Half Stack application development',
     parts: [
@@ -16,6 +24,13 @@ const App = () => {
 
   return (
     <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
       <Header courseTitle={course.name} />
       <Content courseParts={course.parts} />
       <Total courseParts={course.parts} />
